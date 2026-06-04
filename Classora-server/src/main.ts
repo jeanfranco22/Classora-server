4b375cb2810f0908730a62ba2b02a6553dfb2a15
JFFerrerMendez <francoferrer.dev@gmail.com>
2026-06-04 08:42:35 -0600

    fix/cors

--- Classora-server/src/main.ts
+++ Classora-server/src/main.ts
@@ -3,15 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const allowedOrigins = [
     'http://localhost:3000',
     process.env.FRONTEND_URL,
   ].filter((origin): origin is string => Boolean(origin));
 
+  console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
+  console.log('Allowed Origins:', allowedOrigins);
+
   app.enableCors({
     origin: allowedOrigins,
     credentials: true,
   });
+
   await app.listen(process.env.PORT || 3030);
 }
 bootstrap();
